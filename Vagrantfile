ae0792137ff3e6bb8bfd9577cd918a0e92d83ec4
Alvaro Miranda Aguilera <kikitux@gmail.com>
2017-04-28 15:45:04 +0200

    update

--- Vagrantfile
+++ Vagrantfile
@@ -1,7 +1,5 @@
 Vagrant.configure(2) do |config|
   config.vm.provider "virtualbox"
-  config.vm.box = "xenial"
-  config.vm.box_url = "https://cloud-images.ubuntu.com/xenial/current/xenial-server-cloudimg-amd64-vagrant.box"
-  config.vm.synced_folder ".", "/vagrant", disabled: true
+  config.vm.box = "cbednarski/ubuntu-1604"
   config.vm.provision "shell", path: "scripts/provision.sh" , privileged: false
 end
--- scripts/provision.sh
+++ scripts/provision.sh
@@ -2,27 +2,21 @@
 
 # curl -sSL https://raw.githubusercontent.com/kikitux/xenial-gcc6/master/gcc-6_ubuntu.sh | bash
 
-which gcc-6 g++-6 gfortran-6
-if [ $? -eq 0 ] ; then
-  export DEBIAN_FRONTEND=noninteractive
-  sudo apt-get update                                                                                                                                                                                                                     
-  sudo apt-get install -y software-properties-common                                                                                                                                                                     
-  sudo add-apt-repository -y ppa:ubuntu-toolchain-r/test 2>&1
-  sudo apt-get update                                                                                                                                                                                                                     
-  sudo apt-get install -y gcc-6 g++-6 gfortran-6                                                                                                                                                                                          
-  which gcc || sudo update-alternatives --remove-all gcc                                                                                                                                                                                               
-  which g++ || sudo update-alternatives --remove-all g++                                                                                                                                                                                               
-  which gfortran || sudo update-alternatives --remove-all gfortran                                                                                                                                                                                          
-  sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-6 20                                                                                                                                                                   
-  sudo update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-6 20                                                                                                                                                                   
-  sudo update-alternatives --install /usr/bin/gfortran gfortran /usr/bin/gfortran-6 20                                                                                                                                                    
-  sudo update-alternatives --config gcc                                                                                                                                                                                                   
-  sudo update-alternatives --config g++                                                                                                                                                                                                   
-  sudo update-alternatives --config gfortran                                                                                                                                                                                              
-else 
-  sudo apt-get update
-  sudo apt-get install --only-upgrade -y gcc-6 g++-6 gfortran-6
-fi
+export DEBIAN_FRONTEND=noninteractive
+sudo apt-get update                                                                                                                                                                                                                     
+sudo apt-get install -y software-properties-common                                                                                                                                                                     
+sudo add-apt-repository -y ppa:ubuntu-toolchain-r/test 2>&1
+sudo apt-get update                                                                                                                                                                                                                     
+sudo apt-get install -y gcc-6 g++-6 gfortran-6                                                                                                                                                                                          
+which gcc 2>/dev/null || sudo update-alternatives --remove-all gcc 2>/dev/null
+which g++ 2>/dev/null || sudo update-alternatives --remove-all g++ 2>/dev/null
+which gfortran 2>/dev/null || sudo update-alternatives --remove-all gfortran 2>/dev/null
+sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-6 20                                                                                                                                                                   
+sudo update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-6 20                                                                                                                                                                   
+sudo update-alternatives --install /usr/bin/gfortran gfortran /usr/bin/gfortran-6 20                                                                                                                                                    
+sudo update-alternatives --config gcc                                                                                                                                                                                                   
+sudo update-alternatives --config g++                                                                                                                                                                                                   
+sudo update-alternatives --config gfortran                                                                                                                                                                                              
 
 gcc --version
 g++ --version
